a7110eabb782d29fb5b345465a352c42b387f669
NDONGI <103043488+NDONGI@users.noreply.github.com>
2022-04-05 14:17:38 +0200

    Add files via upload

---
+++ c4.js
@@ -0,0 +1,17 @@
+function box(){
+
+        var txt  = confirm("Procced");
+        if(txt=cancel)
+        {
+           
+        
+        } else {
+	      txt="cancel";
+
+        }
+		
+		var txt  = cancel("cancel");
+		
+}
+
+
